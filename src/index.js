b3d6eea7769dba22db6ec134873ad7a1396ee0fd
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-05-11 13:06:25 -0400

    Create function to pull the weather based on location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+//visual crossing api details
+const API_KEY = "CZB8FQH63WGKAKCPYE7XCC2MQ";
+const BASE_URL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+async function getWeather(location) {
+  const response = await fetch(`${BASE_URL}${location}?key=${API_KEY}`);
+
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  const result = await response.json();
+
+  return result;
+}
+
+getWeather("las vegas").then((result) => console.log(result));
